4c5a2800bf17440823753eecf2d5edb8795e0b21
alonzomamani97-cmd <tu_email_de_github@ejemplo.com>
2026-09-13 23:40:51 -0500

    Añadir rutas de login y register a userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Rutas generales
 router.get('/', userController.getUsers);
-
-// Rutas de autenticación (que coinciden con tu frontend auth.js)
 router.post('/login', userController.login);
-router.post('/register', userController.register); // O createUser, dependiendo de cómo lo nombraste en tu controller
+router.post('/register', userController.register);
 
 module.exports = router;
